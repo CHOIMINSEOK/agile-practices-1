c983562c5a6184acb2279e93e9db96185ef55575
aki <>
2019-10-16 10:58:44 +0900

    no message

--- source/GettingItWorking/simple.test.js
+++ source/GettingItWorking/simple.test.js
@@ -1,72 +1,72 @@
 describe("simple shields", function() {
-  var shields
+  //var shields
   var ship
 
   beforeEach(function() {
-    shields = new Shields()
+    //shields = new Shields()
     ship = new Ship()
   })
 
   it("starts down", function() {
-    expect(shields.isUp()).toBe(false)
+    expect(ship.shields.isUp()).toBe(false)
   })
 
   it("can be raised", function() {
-    shields.raise()
-    expect(shields.isUp()).toBe(true)
+    ship.shields.raise()
+    expect(ship.shields.isUp()).toBe(true)
   })
 
   it("check ship energy", function() {
-    expect(ship.getEnergy() == 40000).toBe(true)
+    expect(ship.getEnergy()).toBe(40000)
   })
 
   it("check shield energy", function() {
-    expect(shields.getEnergy() == 2000).toBe(true)
+    expect(ship.shields.getEnergy()).toBe(2000)
   })
 
   it("decrease ship energy", function() {
     ship.decreaseShipEnergy(100)
-    expect(ship.getEnergy() == 39900).toBe(true)
+    expect(ship.getEnergy()).toBe(39900)
   })
 
   it("increase shield energy", function() {
-    shields.increaseShieldsEnergy(100)
-    expect(shields.getEnergy() == 2100).toBe(true)
+    ship.shields.increaseShieldsEnergy(100)
+    expect(ship.shields.getEnergy()).toBe(2100)
   })
 
   it("transfer shield energy", function() {
     ship.transferEnergy(100)
 
-    expect(ship.getEnergy() == 39900).toBe(true)
-    expect(ship.shields.getEnergy() == 2100).toBe(true)
+    expect(ship.getEnergy()).toBe(39900)
+    expect(ship.shields.getEnergy()).toBe(2100)
   })
 
   it("validate shield max energy", function() {
-    expect(ship.shields.increaseShieldsEnergy(12000) === 8000).toBe(true)
-    expect(ship.shields.getEnergy() === 10000).toBe(true)
+    expect(ship.shields.increaseShieldsEnergy(12000)).toBe(8000)
+    expect(ship.shields.getEnergy()).toBe(10000)
   })
 
   it("validate shield max energy", function() {
     ship.transferEnergy(12000)
-    expect(ship.getEnergy() == 32000).toBe(true)
-    expect(ship.shields.getEnergy() === 10000).toBe(true)
+    expect(ship.getEnergy()).toBe(32000)
+    expect(ship.shields.getEnergy()).toBe(10000)
   })
 
   it("shields get damage below its energy", function() {
-    shields.decreaseShieldsEnergy(1500)
-    expect(shields.getEnergy() === 500).toBe(true)
+    ship.shields.decreaseShieldsEnergy(1500)
+    expect(ship.shields.getEnergy()).toBe(500)
   })
 
   it("shields get damage over its energy", function() {
-    expect(shields.decreaseShieldsEnergy(2500) === 500).toBe(true)
-    expect(shields.getEnergy() === 0).toBe(true)
+    expect(ship.shields.decreaseShieldsEnergy(2500)).toBe(500)
+    expect(ship.shields.getEnergy()).toBe(0)
     expect(ship.shields.buckled()).toBe(true)
   })
 
   it("ship attacked", function() {
     ship.shields.raise()
     ship.attacked(1500)
-    expect(ship.shields.getEnergy() === 500)
+    expect(ship.shields.getEnergy()).toBe(500)
     expect(ship.shields.buckled()).toBe(false)
   })
 })
